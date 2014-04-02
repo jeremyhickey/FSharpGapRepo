f4e4a82dc8d772e7915841d360eb3a761ddf16e0
jeremyhickey <jeremy.hickey@gmail.com>
2014-04-02 19:26:52 -0300

    Created tutorial project

---
+++ TryFSharp_BeginningFSharp/TryFSharp_BeginningFSharp/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+    let add x y =
+        x + y
+    let sum = add 2 2
+
+    printf "%d" sum
+    System.Console.ReadLine() |> ignore
+    0 // return an integer exit code
